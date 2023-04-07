71c0c2de9162f14f0a1c28a91dd1af56652fac7c
JadenKeller <jadkeller@csumb.edu>
2023-04-07 15:55:06 -0700

    Add Assignment functionality

--- src/components/Assignment.js
+++ src/components/Assignment.js
@@ -6,7 +6,8 @@ import Cookies from 'js-cookie';
 import Button from '@mui/material/Button';
 import Radio from '@mui/material/Radio';
 import {DataGrid} from '@mui/x-data-grid';
-import {SERVER_URL} from '../constants.js'
+import {SERVER_URL} from '../constants.js';
+import AddAssignment from './AddAssignment.js';
 
 // NOTE:  for OAuth security, http request must have
 //   credentials: 'include' 
@@ -83,10 +84,44 @@ class Assignment extends React.Component {
                     variant="outlined" color="primary" disabled={this.state.assignments.length===0}  style={{margin: 10}}>
               Grade
             </Button>
+            <AddAssignment  add={this.newAssignment} />
             <ToastContainer autoClose={1500} /> 
           </div>
       )
   }
+
+  newAssignment = (assignment) => {
+    const token = Cookies.get('XSRF-TOKEN');
+    fetch(`${SERVER_URL}/assignment`, 
+      {  
+        method: 'POST', 
+        headers: 
+        { 
+          'X-XSRF-TOKEN': token,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(assignment)
+      } )
+    .then(res => {
+        if (res.ok) {
+          toast.success("Assignment successfully added", {
+              position: toast.POSITION.BOTTOM_LEFT
+          });
+          this.fetchAssignments();
+        } else {
+          toast.error("Error new assignment failed.", {
+              position: toast.POSITION.BOTTOM_LEFT
+          });
+          console.error('Post http status =' + res.status);
+        }})
+    .catch(err => {
+      toast.error("Error new assignment failed.", {
+            position: toast.POSITION.BOTTOM_LEFT
+        });
+        console.error(err);
+    })
+  }
+
 }  
 
 export default Assignment;
